befb9536e4bbef76b86e63ceba01b6b52ae10558
alec <alp.plotkin@gmail.com>
2021-06-14 20:26:37 -0400

    changed output format of calculate_delta.R to give a list of tibbles. Initiated sc_model.R to return standard curve linear models.

--- R/calculate_delta.R
+++ R/calculate_delta.R
@@ -6,5 +6,8 @@ calculate_delta <- function(data, time_points = seq_len(nrow(data))) {
   avg_delta <- apply(delta, 2, mean, na.rm = TRUE)
   sd_delta <- apply(delta, 2, sd, na.rm = TRUE)
   cv_delta <- sd_delta/avg_delta * 100
-  list(deltas = delta, avg = avg_delta, cv = cv_delta)
+  # format output
+  avg <- tibble(location = names(avg_delta), avg = avg_delta)
+  cv <- tibble(location = names(cv_delta), cv = cv_delta)
+  list(deltas = delta, avg = avg, cv = cv)
 }
---
+++ R/sc_model.R
@@ -0,0 +1,4 @@
+sc_model <- function(df, expected, actual) {
+  if (!is.data.frame(df))
+    stop("`df` must be a data.frame")
+}
